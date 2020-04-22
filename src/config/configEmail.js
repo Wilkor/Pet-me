2b361919ebbea17e8b272ce32904cbdb15fc128f
Wilkor <34819624+Wilkor@users.noreply.github.com>
2020-04-21 22:10:22 -0300

    Update configEmail.js

--- src/config/configEmail.js
+++ src/config/configEmail.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 
-     sendgrigKey:'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',
+     sendgrigKey:'',
      msgWelcome:' Olá, <strong> {0}</strong> seja bem vindo ao Pet-me, a plataforma que irá conectar você ao seu futuro Pet!',
 
 }
